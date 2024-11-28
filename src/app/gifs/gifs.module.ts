db99c1631fb42998592ec8df06183bf0783c0930
Calixto Bocamba <nicolas3jdd@gmail.com>
2024-11-28 11:22:19 +0100

    all updates

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePagesComponent } from './pages/home/home-pages.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { CardListComponent } from './components/card-list/card-list.component';
+import { CardComponent } from './components/card/card.component';
+
 
 
 
 @NgModule({
   declarations: [
-    HomePagesComponent, SearchBoxComponent, CardListComponent,
+    HomePagesComponent, SearchBoxComponent, CardListComponent, CardComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule, SharedModule
   ],
   exports: [HomePagesComponent]
 })
